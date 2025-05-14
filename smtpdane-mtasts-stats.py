cff14862d99c7470e82cf263a89930428b2d7899
Jörg Schneider <js@joergschneider.com>
2025-05-14 20:50:01 +0200

    add total numbers to diagram titles

--- smtpdane-mtasts-stats.py
+++ smtpdane-mtasts-stats.py
@@ -34,12 +34,12 @@ print("Common mailservers")
 print("mailservers,dane_or_sts,dane_or_sts%,mx_auth,mx_auth%,mx_tlsa,mx_tlsa%,daneonly,stsonly,both,daneonly%,stsonly%,both%")
 print(f"{n},{nany},{nany/n:.1%},{nmxauth},{nmxauth/n:.1%},{nmxtlsa},{nmxtlsa/n:.1%},{ndaneonly},{nstsonly},{nboth},{ndaneonly/nany:.1%},{nstsonly/nany:.1%},{nboth/nany:.1%}")
 
-def autopct(p, n):
+def autopct(p: float, n: int):
     return f"{p:1.1f}% ({n*p/100:1.0f})"
     #return f"{n*p/100:1.0f}\n{p:1.1f}%"
 
 if len(sys.argv) >= 3:
-    subtitle = sys.argv[3] if len(sys.argv) >= 4 else ''
+    subtitle = sys.argv[3] if len(sys.argv) >= 4 else sys.argv[1] # subtitle defaults to input filename
     tab = plt.color_sequences["tab20c"]
     colors = [tab[i] for i in [4, 8]]
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 4), layout='constrained')
@@ -47,19 +47,19 @@ if len(sys.argv) >= 3:
     #fig.subplots_adjust(wspace=.5)
     labels = ['not published', 'published']
     ax1.pie([n - nany, nany], autopct=lambda p: autopct(p, n), labels=labels, colors=colors, explode=[0, .2], startangle=360*nany/n/2)
-    ax1.set_title("Email providers publishing SMTP TLS policies\n(SMTP DANE or MTA STS)")
+    ax1.set_title(f"Email providers publishing SMTP TLS policies\n(SMTP DANE or MTA STS)\n{n}")
 
     labels = ['SMTP DANE only', 'both standards', 'MTA STS only']
     colors = [tab[i] for i in [9, 8, 10]]
     ax2.pie([ndaneonly, nboth, nstsonly], autopct=lambda p: autopct(p, nany), colors=colors)
-    ax2.set_title("SMTP TLS policy publication details\n(SMTP DANE and/or MTA STS)")
+    ax2.set_title(f"SMTP TLS policy publication details\n(SMTP DANE and/or MTA STS)\n{nany}")
     ax2.legend(labels, title="Legend",
             loc="center left",
             bbox_to_anchor=(1, 0, 0.5, 1))
     
     labels = ['MX TLSA only', 'SMTP DANE', 'MX Auth only']
     ax3.pie([nmxtlsaonly, ndane, nmxauthonly], autopct=lambda p: autopct(p, npartialdane), colors=colors)
-    ax3.set_title("Partial SMTP DANE\n(Full SMTP DANE requires DNSSEC for MX lookup and TLSA records for MXs)")
+    ax3.set_title(f"Partial SMTP DANE configurations\n(Full SMTP DANE requires DNSSEC for MX lookup and TLSA records for MXs)\n{npartialdane}")
     ax3.legend(labels, title="Legend",
             loc="center left",
             bbox_to_anchor=(1, 0, 0.5, 1))
