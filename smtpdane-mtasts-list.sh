893531bf90615bfa25be4bf25bc5424dbb8a56b6
Jörg Schneider <js@joergschneider.com>
2025-07-09 23:01:12 +0200

    use --flush

--- smtpdane-mtasts-list.sh
+++ smtpdane-mtasts-list.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-{ 
-    smtpdane-mtasts-lookup.py -H
-    sort -u "$@" | xargs --max-args 100 --max-procs 100 smtpdane-mtasts-lookup.py --delimiter '|' #| sort
-}
+smtpdane-mtasts-lookup.py -H
+sort -u "$@" | xargs --max-args 100 --max-procs 100 smtpdane-mtasts-lookup.py --delimiter '|' --flush | sort
+#sort -u "$@" | xargs smtpdane-mtasts-lookup.py --delimiter '|' --flush #| sort
